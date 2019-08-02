39da5ddeaafe090e7a5f0e8e21353151444be713
Aravindan Jayaraju <aravindanj@yahoo.com>
2019-08-02 11:28:49 +0100

    with change 2

--- src/com/gitRepoTest/GitTest.java
+++ src/com/gitRepoTest/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello...this is a test");
 		System.out.println("change 1");
+		System.out.println("change 2");
+
 
 	}
 
